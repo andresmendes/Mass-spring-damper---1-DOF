a7208eb3bb02e4774b26ef8b1e728a6515f9d722
André de Souza Mendes <asmendes@fei.edu.br>
2021-12-17 09:05:08 -0300

    Update mass_spring_damper_1_dof.m

--- mass_spring_damper_1_dof.m
+++ mass_spring_damper_1_dof.m
@@ -15,12 +15,12 @@ c = 4000;                       % Damping constant              [N.s/m]
 
 % Animation model
 L0  = 0.7;                      % Spring relaxed length         [m]
-h   = 0.3;                      % Height of the block           [m]
-a   = 1;                        % Width of the block            [m]
+h   = 0.4;                      % Height of the block           [m]
+a   = 0.8;                      % Width of the block            [m]
 
 % Video
-tF      = 30;                   % Final time                    [s]
-fR      = 30;                   % Frame rate                    [fps]
+tF      = 15;                   % Final time                    [s]
+fR      = 60;                   % Frame rate                    [fps]
 dt      = 1/fR;                 % Time resolution               [s]
 time    = linspace(0,tF,tF*fR); % Time                          [s]
 
@@ -28,7 +28,7 @@ time    = linspace(0,tF,tF*fR); % Time                          [s]
 A = 0.1;                        % Amplitude                     [m]
 f = 1.0;                        % Frequency                     [Hz]
 w = 2*pi*f;                     % Frequency                     [rad/s]
-u_vet = A*cos(w*time');          % Displacement                  [m]
+u_vet = A*cos(w*time');         % Displacement                  [m]
 
 %% Simulation
 
@@ -40,30 +40,35 @@ G = tf([c k],[m c k]);
 
 %% Animation
 
+color = cool(6); % Colormap
+
 % Mass absolute vertical position (lower center point)
 yc = y(:,1) + L0; 
 
 % Base info
-baseX = [-0.8 0.8];
+baseX = [-0.3 0.3];
 baseY = [u_vet u_vet];
 
 figure
-set(gcf,'Position',[50 50 1280 720]) % 720p
-% set(gcf,'Position',[50 50 854 480]) % 480p
+% set(gcf,'Position',[50 50 1280 720])  % YouTube: 720p
+% set(gcf,'Position',[50 50 854 480])   % YouTube: 480p
+set(gcf,'Position',[50 50 640 640])     % Social
+
+hold on ; grid on ; axis equal
+set(gca,'ylim',[-0.2 1.35],'xlim',[-0.8 0.8],'xtick',-0.8:0.2:0.8,'ytick',-0.2:0.2:1.3)
+set(gca,'FontName','Verdana','FontSize',16)
 
 % Create and open video writer object
-v = VideoWriter('mass_spring_damper_1_dof_base.avi');
+v = VideoWriter('mass_spring_damper_1_dof_base.mp4','MPEG-4');
 v.Quality   = 100;
 v.FrameRate = fR;
 open(v);
 
 for i=1:length(time)
     cla
-    hold on ; grid on ; axis equal
-    set(gca,'ylim',[-0.2 1.3],'xlim',[-1 1],'xtick',-1:0.1:1,'ytick',-0.2:0.1:1.3)
     
     % Mass plot
-    fill([-a/2 a/2 a/2 -a/2],[yc(i) yc(i) yc(i)+h yc(i)+h],'r')
+    fill([-a/2 a/2 a/2 -a/2],[yc(i) yc(i) yc(i)+h yc(i)+h],color(6,:),'LineWidth',2)
     
     % Base position instant
     baseYval = baseY(i,:);
@@ -79,14 +84,14 @@ for i=1:length(time)
     % Amplitude markers
     % Find steady state amplitude during second half of the simulation:
     ycSteady = yc(floor(end/2):end);
-    plot([-0.8 0.8],[min(ycSteady) min(ycSteady)],'k--')
-    plot([-0.8 0.8],[max(ycSteady) max(ycSteady)],'k--')
+    plot([-0.6 0.6],[min(ycSteady) min(ycSteady)],'k--','LineWidth',1.5)
+    plot([-0.6 0.6],[max(ycSteady) max(ycSteady)],'k--','LineWidth',1.5)
 
     % Amplitude position input
-    plot([-0.8 0.8],[A A],'k--')
-    plot([-0.8 0.8],[-A -A],'k--')
+    plot([-0.6 0.6],[A A],'k--','LineWidth',1.5)
+    plot([-0.6 0.6],[-A -A],'k--','LineWidth',1.5)
 
-    title(strcat('Mass spring damper: m=',num2str(m),' kg ; k=',num2str(k),' N/m ; c=',num2str(c),' N.s/m'))
+    title('Mass spring damper - 1 DOF')
     
     xlabel('x [m]')
     ylabel('y [m]')
@@ -107,7 +112,8 @@ function plotSpring(L0,yb,yc,i)
 
     rodPct      = 0.11;     % Length rod percentage of L0 
     springPct   = 1/3;      % Spring pitch percentage of L0 
-
+    spring_wid  = 3;        % Spring line width
+    
     % Spring length without rods
     L = (yc - yb) - 2*rodPct*L0;
     
@@ -115,62 +121,36 @@ function plotSpring(L0,yb,yc,i)
     center  = -0.2;         % Lateral position
     wid     = 0.1;          % Width
 
-    % rod attached to base
-    rod1X = [center center];
-    rod1Y = [yb yb+rodPct*L0];
-    % rod attached to mass
-    rod2X = [center center];
-    rod2Y = [yc yc-rodPct*L0];
-
-    % Spring parts
-    % Part 1
-    spring1X = [center center+wid];
-    spring1Y = [yb+rodPct*L0 yb+rodPct*L0];
-    % Part 2
-    spring2X = [center-wid center+wid];
-    spring2Y = [yb+rodPct*L0+springPct*L yb+rodPct*L0];
-    % Part 3
-    spring3X = [center-wid center+wid];
-    spring3Y = [yb+rodPct*L0+1*springPct*L yb+rodPct*L0+1*springPct*L];
-    % Part 4
-    spring4X = [center-wid center+wid];
-    spring4Y = [yb+rodPct*L0+2*springPct*L yb+rodPct*L0+1*springPct*L];
-    % Part 5
-    spring5X = [center-wid center+wid];
-    spring5Y = [yb+rodPct*L0+2*springPct*L yb+rodPct*L0+2*springPct*L];
-    % Part 6
-    spring6X = [center-wid center+wid];
-    spring6Y = [yb+rodPct*L0+3*springPct*L yb+rodPct*L0+2*springPct*L];
-    % Part 7
-    spring7X = [center-wid center];
-    spring7Y = [yb+rodPct*L0+3*springPct*L yb+rodPct*L0+3*springPct*L];
+    % Spring
+    springX = [ 
+                center                              % Start
+                center                              % rod
+                center+wid                          % Part 1   
+                center-wid                          % Part 2
+                center+wid                          % Part 3
+                center-wid                          % Part 4
+                center+wid                          % Part 5
+                center-wid                          % Part 6
+                center                              % Part 7
+                center                              % rod/End
+                ];
     
-    % Iteration values
-    % rods
-    rod1Yval = rod1Y(i,:);
-    rod2Yval = rod2Y(i,:);
-    % Spring parts
-    spring1Yval = spring1Y(i,:);
-    spring2Yval = spring2Y(i,:);
-    spring3Yval = spring3Y(i,:);
-    spring4Yval = spring4Y(i,:);
-    spring5Yval = spring5Y(i,:);
-    spring6Yval = spring6Y(i,:);
-    spring7Yval = spring7Y(i,:);
+	springY = [ 
+                yb(i)                               % Start
+                yb(i)+rodPct*L0                     % rod
+                yb(i)+rodPct*L0                     % Part 1 
+                yb(i)+rodPct*L0+springPct*L(i)      % Part 2
+                yb(i)+rodPct*L0+springPct*L(i)      % Part 3
+                yb(i)+rodPct*L0+2*springPct*L(i)    % Part 4
+                yb(i)+rodPct*L0+2*springPct*L(i)    % Part 5
+                yb(i)+rodPct*L0+3*springPct*L(i)    % Part 6
+                yb(i)+rodPct*L0+3*springPct*L(i)    % Part 7
+                yb(i)+2*rodPct*L0+3*springPct*L(i)  % rod/End
+               ]; 
 
     % PLOT
-    % rods
-    plot(rod1X,rod1Yval,'k')
-    plot(rod2X,rod2Yval,'k')
-    % Spring parts
-    plot(spring1X,spring1Yval,'k')
-    plot(spring2X,spring2Yval,'k')
-    plot(spring3X,spring3Yval,'k')
-    plot(spring4X,spring4Yval,'k')
-    plot(spring5X,spring5Yval,'k')
-    plot(spring6X,spring6Yval,'k')
-    plot(spring7X,spring7Yval,'k')
-
+    plot(springX,springY,'k','LineWidth',spring_wid)
+        
 end
 
 function plotDamper(L0,yb,yc,i)
@@ -178,7 +158,8 @@ function plotDamper(L0,yb,yc,i)
     rodLowerPct = 0.1; % Length lower rod percentage of L0  
     rodUpperPct = 0.6; % Length upper rod percentage of L0 
     cylinderPct = 0.6; % Length cylinder porcentagem de L0
-
+    damper_line_wid  = 3;   % Damper line width
+    
     % Damper geometry
     center  = 0.2;              % Lateral position
     wid     = 0.05;             % Width
@@ -186,15 +167,21 @@ function plotDamper(L0,yb,yc,i)
     % rod attached to base
     rod1X = [center center];
     rod1Y = [yb yb+rodLowerPct*L0];
+    
     % Damper base cylinder - rod - base 
-    basecylinderX = [center-wid center+wid];
-    basecylinderY = [yb+rodLowerPct*L0 yb+rodLowerPct*L0];
-    % Damper left wall cylinder
-    wall_1CylinderX = [center-wid center-wid];
-    wall_1CylinderY = [yb+rodLowerPct*L0 yb+rodLowerPct*L0+cylinderPct*L0];
-    % Damper right wall cylinder
-    wall_2CylinderX = [center+wid center+wid];
-    wall_2CylinderY = [yb+rodLowerPct*L0 yb+rodLowerPct*L0+cylinderPct*L0];
+    cylinderX = [   
+                    center-wid
+                    center-wid
+                    center+wid
+                    center+wid
+                ];
+                
+    cylinderY = [
+                    yb(i)+rodLowerPct*L0+cylinderPct*L0
+                    yb(i)+rodLowerPct*L0 
+                    yb(i)+rodLowerPct*L0 
+                    yb(i)+rodLowerPct*L0+cylinderPct*L0
+                ];
     
     % rod attached to mass
     rod2X = [center center];
@@ -207,18 +194,13 @@ function plotDamper(L0,yb,yc,i)
     rod1Yval = rod1Y(i,:);
     rod2Yval = rod2Y(i,:);
     pistonYVal = pistonY(i,:);
-    baseCylinderYval = basecylinderY(i,:);
-    wall_1CylinderYval = wall_1CylinderY(i,:);
-    wall_2CylinderYval = wall_2CylinderY(i,:);
 
     % PLOT
     % rods
-    plot(rod1X,rod1Yval,'k')
-    plot(rod2X,rod2Yval,'k')
+    plot(rod1X,rod1Yval,'k','LineWidth',damper_line_wid)
+    plot(rod2X,rod2Yval,'k','LineWidth',damper_line_wid)
     % Damper parts
-    plot(pistonX,pistonYVal,'k')
-    plot(basecylinderX,baseCylinderYval,'k')
-    plot(wall_1CylinderX,wall_1CylinderYval,'k')
-    plot(wall_2CylinderX,wall_2CylinderYval,'k')
+    plot(pistonX,pistonYVal,'k','LineWidth',damper_line_wid)
+    plot(cylinderX,cylinderY,'k','LineWidth',damper_line_wid)
 
 end
